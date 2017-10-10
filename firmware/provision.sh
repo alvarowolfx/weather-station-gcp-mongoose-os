a27b060c2243bde27c81197ac7904d67f2bd3dd8
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-10-10 09:01:19 -0400

    [chore] Remove unused files

--- firmware/provision.sh
+++ firmware/provision.sh
@@ -1,4 +1,4 @@
 mos build --arch esp32
 mos flash
-mos wifi 'casa viebrantz' alvaro.felipe
+mos wifi ssid password
 mos gcp-iot-setup --gcp-project weather-station-iot-170004 --gcp-region us-central1 --gcp-registry weather-station-registry
