f3daeb5fd207124c882cf81a340671cfe008c30f
Alvaro Viebrantz <alvarowolfx@gmail.com>
2018-01-22 22:06:20 -0300

    [fix] Compilation problem, no need to include mos_gcp.h fixes #1

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,10 +1,9 @@
 
+#include "mgos_app.h"
+
 #include "mgos.h"
-#include "mgos_gcp.h"
 
-enum mgos_app_init_result mgos_app_init(void) {
-
-  mgos_gcp_init();
-
+enum mgos_app_init_result mgos_app_init(void)
+{
   return MGOS_APP_INIT_SUCCESS;
 }
